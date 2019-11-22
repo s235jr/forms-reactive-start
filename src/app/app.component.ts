6e3abfcd69723440bb9bb7c3fcc259970522faaf
Tomek Ciećwierz <tomasz.ciecwierz@gmail.com>
2019-11-22 12:33:37 +0100

    add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl('Username', Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signForm.get('hobbies')).push(control);
+  }
+
+  getControls() {
+    return (<FormArray>this.signForm.get('hobbies')).controls;
+  }
 }
